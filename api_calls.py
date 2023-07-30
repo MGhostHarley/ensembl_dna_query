0e37d1896a0f03aa7aabc9e43a630833cd61dbcd
Michael Harley <m.harley267@gmail.com>
2023-07-30 16:41:53 -0700

    refactored code from spaghetti code to functions

--- api_calls.py
+++ api_calls.py
@@ -4,6 +4,8 @@ import json
 import os.path
 
 import requests
+
+from typing import List
 from pprint import pprint as pp
 
 
@@ -27,47 +29,144 @@ MONDAY
 
 """
 
-# PARSER
 
-
+################# HELPER FUNCTIONS #################
 def is_valid_file(arg: str) -> bool:
     if not os.path.isfile(arg) and not arg.endswith(".txt"):
         return False
-
     return True
 
 
-parser = argparse.ArgumentParser(
-    description="Get variant files",
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-)
-parser.add_argument(
-    "file_path", help="File path of variant file. Must be .txt", type=str
-)
+######################## IO #######################
+def open_file(file_name: str) -> List[str]:
+    with open(file_name) as file:
+        variants = [line.rstrip("\n") for line in file]
+    return variants
 
 
-args = parser.parse_args()
-config = vars(args)
-file_name = config.get("file_path")
+def output_results(results: List[List[dict]]) -> None:
+    result, errors = results
+    with open("output.tsv", "w") as output_file:
+        dw = csv.DictWriter(output_file, sorted(result[0].keys()), delimiter="\t")
+        dw.writeheader()
+        dw.writerows(result)
 
-if not is_valid_file(config.get("file_path", None)):
-    parser.error(
-        f"The file { config.get('file_path', None)} does not exist or is not a .txt file"
+    with open("error.tsv", "w") as error_file:
+        dw = csv.DictWriter(error_file, sorted(errors[0].keys()), delimiter="\t")
+        dw.writeheader()
+        dw.writerows(errors)
+
+
+def command_parser() -> str:
+    parser = argparse.ArgumentParser(
+        description="Get variant files",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "file_path",
+        help="File path of variant file. Must be .txt and absolute file_path",
+        type=str,
     )
 
-print(config)
+    args = parser.parse_args()
+    config = vars(args)
 
-# READ FILES
-with open(file_name) as file:
-    variants = [line.rstrip("\n") for line in file]
-# print(variants)
+    if not is_valid_file(config.get("file_path", None)):
+        parser.error(
+            f"""The file { config.get('file_path', None)}
+            
+            1. Does not exist
+            2. Is not a .txt file
+            
+            Please use absolute file_path and .txt file.
+            """
+        )
 
-# API CALLS HERE
+    return config.get("file_path", "")
 
 
-request_list = []
-error_list = []
-headers = {"Content-Type": "application/json"}
+######################## API CALLS #######################
+def parse_ensemble_response(response_list: List[List[dict]]) -> List[dict]:
+    reduced_list = []
+    flattened = [response for responses in response_list for response in responses]
+
+    for flat in flattened:
+        reduced_list.append(
+            {
+                "input": flat.get("input", None),
+                "assembly_name": flat.get("assembly_name", None),
+                "seq_region_name": flat.get("seq_region_name", None),
+                "start": flat.get("start", None),
+                "end": flat.get("end", None),
+                "strand": flat.get("strand", None),
+                "most_severe_consequence": flat.get("most_severe_consequence", None),
+                "gene": flat.get("transcript_consequences", None)[0].get(
+                    "gene_symbol", None
+                ),
+            }
+        )
+    pp(reduced_list)
+    return reduced_list
+
+
+def query_ensemble_api(variants: List[str]) -> List[dict]:
+    response_list = []
+    error_list = []
+
+    headers = {"Content-Type": "application/json"}
+    base_url = r"http://rest.ensembl.org/vep/human/hgvs/"
+
+    for variant in variants:
+        url = base_url + variant
+        print(url)
+        try:
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
+
+        except requests.exceptions.HTTPError as http_error:
+            error_list.append(
+                {
+                    "variant": variant,
+                    "http_error": http_error,
+                    "error_description": response.json(),
+                }
+            )
+            print("Http Error:", http_error)
+            continue
+
+        except requests.exceptions.ConnectionError as connection_error:
+            error_list.append({"variant": variant, "error": connection_error})
+            print("Error Connecting:", connection_error)
+
+        except requests.exceptions.Timeout as timeout_error:
+            error_list.append({"variant": variant, "error": timeout_error})
+            print("Timeout Error:", timeout_error)
+
+        except requests.exceptions.RequestException as request_error:
+            error_list.append({"variant": variant, "error": request_error})
+            print("OOps: Something Else", request_error)
+
+        formatted_response = response.json()
+        response_list.append(formatted_response)
+
+    parsed_list = parse_ensemble_response(response_list)
+    return parsed_list, error_list
+
+
+def main() -> None:
+    file_path = command_parser()
+    print(file_path)
+
+    file_contents = open_file(file_path)
+    print(file_contents)
+
+    results = query_ensemble_api(file_contents)
+    print(results)
+    output_results(results)
+
+
+main()
+
 # # url = r"http://rest.ensembl.org/vep/human/hgvs/NC_000001.11:g.40819893T>A"
 # # # url = r"http://rest.ensembl.org/vep/human/hgvs/NC_000006.12:g.152387156G>A"
 
@@ -80,83 +179,3 @@ headers = {"Content-Type": "application/json"}
 
 # # except requests.exceptions.HTTPError as errh:
 # #     print("Http Error:", errh)
-
-
-for variant in variants:
-    url = r"http://rest.ensembl.org/vep/human/hgvs/" + variant
-    print(url)
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
-
-    except requests.exceptions.HTTPError as http_error:
-        error_list.append(
-            {
-                "variant": variant,
-                "http_error": http_error,
-                "error_description": response.json(),
-            }
-        )
-        print("Http Error:", http_error)
-        continue
-
-    except requests.exceptions.ConnectionError as connection_error:
-        error_list.append({"variant": variant, "error": connection_error})
-        print("Error Connecting:", connection_error)
-
-    except requests.exceptions.Timeout as timeout_error:
-        error_list.append({"variant": variant, "error": timeout_error})
-        print("Timeout Error:", timeout_error)
-
-    except requests.exceptions.RequestException as request_error:
-        error_list.append({"variant": variant, "error": request_error})
-        print("OOps: Something Else", request_error)
-
-    formatted_response = response.json()
-    request_list.append(formatted_response)
-
-
-pp(request_list)
-pp(error_list)
-print(len(request_list))
-
-"""
-    input
-    assembly_name
-    seq_region_name
-    start
-    end
-    strand
-    most_severe_consequence
-    transcript_consequences > list > gene_symbol
-"""
-
-
-### PARSING
-flattened = [request for requests in request_list for request in requests]
-pp(flattened)
-
-reduced_list = []
-
-for flat in flattened:
-    reduced_list.append(
-        {
-            "input": flat.get("input", None),
-            "assembly_name": flat.get("assembly_name", None),
-            "seq_region_name": flat.get("seq_region_name", None),
-            "start": flat.get("start", None),
-            "end": flat.get("end", None),
-            "strand": flat.get("strand", None),
-            "most_severe_consequence": flat.get("most_severe_consequence", None),
-            "gene": flat.get("transcript_consequences", None)[0].get(
-                "gene_symbol", None
-            ),
-        }
-    )
-pp(reduced_list)
-
-### OUTPUT FILES
-with open("output.tsv", "w") as output_file:
-    dw = csv.DictWriter(output_file, sorted(reduced_list[0].keys()), delimiter="\t")
-    dw.writeheader()
-    dw.writerows(reduced_list)
