2d41e432c6a6418ea79fe65383950d2ffd36b5f7
Michael Harley <m.harley267@gmail.com>
2023-07-30 17:00:40 -0700

    added  better error handling and created error output file

--- api_calls.py
+++ api_calls.py
@@ -12,9 +12,9 @@ from pprint import pprint as pp
 """
 AFTERNOON
 1. take terminal input XX
-2. refactor code
-3. better error handling
-4. Out put errors to file
+2. refactor code XX
+3. better error handling XX
+4. Out put errors to file XX
 5. generate timestamp for file names
 
 NIGHT:
@@ -37,10 +37,36 @@ def is_valid_file(arg: str) -> bool:
     return True
 
 
+def parse_ensemble_response(response_list: List[List[dict]]) -> List[dict]:
+    reduced_list = []
+    flattened = [response for responses in response_list for response in responses]
+
+    for flat in flattened:
+        reduced_list.append(
+            {
+                "input": flat.get("input", None),
+                "assembly_name": flat.get("assembly_name", None),
+                "seq_region_name": flat.get("seq_region_name", None),
+                "start": flat.get("start", None),
+                "end": flat.get("end", None),
+                "strand": flat.get("strand", None),
+                "most_severe_consequence": flat.get("most_severe_consequence", None),
+                "gene": flat.get("transcript_consequences", None)[0].get(
+                    "gene_symbol", None
+                ),
+            }
+        )
+    pp(reduced_list)
+    return reduced_list
+
+
 ######################## IO #######################
 def open_file(file_name: str) -> List[str]:
     with open(file_name) as file:
         variants = [line.rstrip("\n") for line in file]
+
+    if not variants:
+        raise Exception("File is empty")
     return variants
 
 
@@ -86,39 +112,23 @@ def command_parser() -> str:
 
 
 ######################## API CALLS #######################
-def parse_ensemble_response(response_list: List[List[dict]]) -> List[dict]:
-    reduced_list = []
-    flattened = [response for responses in response_list for response in responses]
-
-    for flat in flattened:
-        reduced_list.append(
-            {
-                "input": flat.get("input", None),
-                "assembly_name": flat.get("assembly_name", None),
-                "seq_region_name": flat.get("seq_region_name", None),
-                "start": flat.get("start", None),
-                "end": flat.get("end", None),
-                "strand": flat.get("strand", None),
-                "most_severe_consequence": flat.get("most_severe_consequence", None),
-                "gene": flat.get("transcript_consequences", None)[0].get(
-                    "gene_symbol", None
-                ),
-            }
-        )
-    pp(reduced_list)
-    return reduced_list
-
-
 def query_ensemble_api(variants: List[str]) -> List[dict]:
     response_list = []
     error_list = []
+
+    if not variants:
+        error_list.append(
+            {
+                "error_description": "No variants were given.",
+            }
+        )
+        return response_list, error_list
 
     headers = {"Content-Type": "application/json"}
     base_url = r"http://rest.ensembl.org/vep/human/hgvs/"
 
     for variant in variants:
         url = base_url + variant
-        print(url)
         try:
             response = requests.get(url, headers=headers)
             response.raise_for_status()
@@ -135,16 +145,21 @@ def query_ensemble_api(variants: List[str]) -> List[dict]:
             continue
 
         except requests.exceptions.ConnectionError as connection_error:
-            error_list.append({"variant": variant, "error": connection_error})
+            error_list.append(
+                {"variant": variant, "connection_error": connection_error}
+            )
             print("Error Connecting:", connection_error)
+            continue
 
         except requests.exceptions.Timeout as timeout_error:
-            error_list.append({"variant": variant, "error": timeout_error})
+            error_list.append({"variant": variant, "timeout_error": timeout_error})
             print("Timeout Error:", timeout_error)
+            continue
 
         except requests.exceptions.RequestException as request_error:
-            error_list.append({"variant": variant, "error": request_error})
+            error_list.append({"variant": variant, "request_error": request_error})
             print("OOps: Something Else", request_error)
+            continue
 
         formatted_response = response.json()
         response_list.append(formatted_response)
